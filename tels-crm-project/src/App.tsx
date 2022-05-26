f14d868b4c996aa8f0fb99b26ee966df17b1d970
bliznekov <r.bliznekov@gmail.com>
2022-05-26 16:44:23 +0300

    feat: add header

--- tels-crm-project/src/App.tsx
+++ tels-crm-project/src/App.tsx
@@ -1,8 +1,18 @@
 import React from "react";
-import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./component/header/Header";
+
+import "./App.scss";
 
 function App() {
-    return <div>Text</div>;
+    return (
+        <BrowserRouter>
+            <div className="app-container">
+                <Header />
+                <div className="container"></div>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ tels-crm-project/src/component/header/Header.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+import c from "./Header.module.scss";
+import LogoIcon from "../../assets/logo.png";
+
+const getLinks = [{ url: "/info", text: "Info" }];
+
+const Header: React.FC = () => {
+    const links = getLinks;
+
+    return (
+        <nav className={c.headerContainer}>
+            <div className={c.container}>
+                <div className={c.logo}>
+                    <img className={c.img} src={LogoIcon} alt={"TELS CARGO"} />
+                    <div className={c.appName}>TELS CARGO</div>
+                </div>
+
+                <ul className={c.links}>
+                    {links.map(({ url, text }) => (
+                        <li key={url + text}>
+                            <NavLink
+                                to={url}
+                                className={({ isActive }) =>
+                                    isActive ? c.active : ""
+                                }
+                            >
+                                {text}
+                            </NavLink>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
